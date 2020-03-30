4b52e805c96f15eabf94960de81e24e4a432bb1e
Chess Seventh <chess7th@pm.me>
2020-03-30 10:57:32 +0200

    Add helper functions for sorting and date normalizing

--- helpers.py
+++ helpers.py
@@ -6,6 +6,9 @@ Github: https://github.com/chess-seventh
 Description: Module for function helpers.
 """
 
+import datetime
+
+
 def sort_task_urgency(task):
     """Helper to sort tasks by urgency.
 
@@ -14,3 +17,35 @@ def sort_task_urgency(task):
 
     """
     return task['urgency']
+
+
+def sort_task_scheduled(task):
+    """Helper to sort tasks by scheduled date.
+
+    :task: Task class.
+    :returns: The tasks' scheduled date.
+
+    """
+    return task['scheduled']
+
+
+def sort_task_due(task):
+    """Helper to sort tasks by due date.
+
+    :task: Task class.
+    :returns: The tasks' due date.
+
+    """
+    return task['due']
+
+
+def normalize_task_date(date, key):
+    """Due/Scheduled dates in TaskWarrior contain the TimeZone. This function
+    normalizes this so we can work properly.
+
+    :date: The date to normalize.
+    :returns: TODO
+
+    """
+    if isinstance(date[key], datetime):
+        return date[key].replace(tzinfo=None)
