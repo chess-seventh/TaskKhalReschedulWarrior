5d652c9f4cfc7456c30ceede005d140517166925
Chess Seventh <chess7th@pm.me>
2020-04-04 18:20:14 +0200

    Refactor days chosing functions

--- events.py
+++ events.py
@@ -10,48 +10,50 @@ import datetime
 from datetime import date, timedelta
 from logger import logger
 from ics import Event
-# from ics import Calendar
+from ics import Calendar
 
 
-def create_event(task):
+def create_event(task, scheduled):
     """Create an Event based on task parameters.
 
     :task: Task to create an event from.
     :returns: Event file
 
     """
+    calendar = Calendar()
     event = Event()
     event.name = task['description']
     event.uid = task['uuid']
+    event.begin = "1"
     # task and event must have same
     # event.begin = datetime.datetime.now()
     # event.end = datetime.datetime.now()+1
 
 
-def next_sunday():
-    """Get future sundays.
-    :returns: List of future sundays.
-
+def next_days(day_of_week):
+    """Get future days.
+    :returns: List of future days.
     """
     today = datetime.datetime.today()
     month = today.month
     year = today.year
 
-    sundays = list()
-    for sunday in month_sundays(year, month):
-        sundays.append(sunday)
+    chosen_day = list()
+    for sunday in month_chosen_day(year, month, chosen_day):
+        chosen_day.append(sunday)
+    return chosen_day
 
 
-def month_sundays(year, month):
+def month_chosen_day(year, month, chosen_day):
     """Yields sundays for current month.
     :returns: List of future sundays.
 
     """
-    date_sun = date(year, month, 1)
-    date_sun += timedelta(days=6 - date_sun.weekday())
-    while date_sun.year == year and date_sun.month == month:
-        yield date_sun
-        date_sun += timedelta(days=7)
+    date_chosen = date(year, month, 1)
+    date_chosen += timedelta(days=7 - date_chosen.weekday())
+    while date_chosen.year == year and date_chosen.month == month:
+        yield date_chosen
+        date_chosen += timedelta(days=7)
 
 
 def event_task_status(task):
