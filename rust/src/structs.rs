d20c5de56e8035ce915735fa6f0deab2852be5f5
Chess Seventh <chess7th@pm.me>
2020-08-02 12:27:00 +0200

    Add Sctruct.rs

---
+++ rust/src/structs.rs
@@ -0,0 +1,32 @@
+use serde::{Deserialize, Serialize};
+use std::fmt;
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+pub struct Task {
+    pub description: String,
+    pub id: i64,
+    pub priority: Option<String>,
+    pub project: String,
+    pub status: String,
+    pub urgency: f64,
+    pub uuid: String
+}
+
+impl fmt::Display for Task {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "description: {}\n
+               id: {}\n
+               project: {}\n
+               status: {}\n
+               urgency: {}\n
+               uuid: {}",
+               self.description,
+               self.id,
+               self.project,
+               self.status,
+               self.urgency,
+               self.uuid
+              )
+    }
+}
+
